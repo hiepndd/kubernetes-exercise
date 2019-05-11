eab76202b9f5cf4046108ffc13780333c4acbbdf
hiepndd <nguyendangdaihiep@gmail.com>
2019-05-11 10:00:13 +0700

    build image

---
+++ docker/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func EchoHandler(writer http.ResponseWriter, request *http.Request) {
+
+	request.Write(writer)
+	writer.Write([]byte("\nGreetings from coderschool.vn V1.0\n"))
+
+}
+
+func main() {
+	log.Println("starting server, listening on port 8080")
+	http.HandleFunc("/", EchoHandler)
+	http.ListenAndServe(":8080", nil)
+}
